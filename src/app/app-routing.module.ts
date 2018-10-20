b033de103df3de738fe978ea670986ab307c5f66
hryniewiczm <hryniewicz.m@o2.pl>
2018-10-20 11:52:55 +0200

    adding launches routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { LaunchesComponent } from './launches/launches.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'launches', component: LaunchesComponent },
+  { path: '', redirectTo: './home', pathMatch: 'full' }
   ];
 
 @NgModule({
