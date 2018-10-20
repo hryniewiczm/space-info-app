a1e8830a3920d795442006e9f09792e918169f64
hryniewiczm <hryniewicz.m@o2.pl>
2018-10-20 13:08:07 +0200

    adding

--- src/app/launch-card/launch-card.component.ts
+++ src/app/launch-card/launch-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-launch-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./launch-card.component.css']
 })
 export class LaunchCardComponent implements OnInit {
+  @Input() launch: Object;
 
   constructor() { }
 
